e8e3d316b677889b5107811863a0c1d2663ff87b
Alexey Kulpin <alexey.kulpin@sealine.su>
2019-12-12 15:54:11 +0300

    Changes: - removed python script use json instead - changed Zabbix version to 4.4

---
+++ externalscripts/list.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /usr/lib/zabbix/externalscripts/
+cat $1
--- externalscripts/sites_list.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.5
-import json
-
-ib_list = []
-ib_list.append({"{#NAME}": "mail.ru", "{#ADDRESS}": "mail.ru", "{#SSL_PORT}": "443"})
-ib_list.append({"{#NAME}": "ya.ru", "{#ADDRESS}": "ya.ru", "{#SSL_PORT}": "443"})
-print(json.dumps({'data': ib_list}))
